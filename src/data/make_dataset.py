24f38b9af22fa5a12fdd8afe9c964b8527a8378e
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2018-08-13 15:48:18 +0200

    :sparkles: Add some visualization

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 
-def load_datasets(nrows=None):
-    train = pd.read_csv('../data/raw/train.csv')
-    test = pd.read_csv('../data/raw/test.csv')
-    return (train, test)
+class Dataset:
+    def __init__(self, nrows=None):
+        train = pd.read_csv('../data/raw/train.csv', nrows=nrows)
+        test = pd.read_csv('../data/raw/test.csv', nrows=nrows)
+        
+        quantitative_cols = [f for f in train.columns if train.dtypes[f] != 'object']
+        quantitative_cols.remove('SalePrice')
+        quantitative_cols.remove('Id')
+        
+        self.data = (train, test)
+        self.quantitative_cols = quantitative_cols
+        self.qualitative_cols = [f for f in train.columns if train.dtypes[f] == 'object']
